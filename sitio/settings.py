f78688169369c0db0643a2f2d41df2e9d683c7af
luisdavidm8 <lmercadom21@curnvirtual.edu.co>
2024-04-15 20:10:18 -0500

    Cambie el HTML para la pagina

--- sitio/settings.py
+++ sitio/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-^3#jgu#%_zrkcc)#-q93stjj9bx5*yt*^gqhh8wzt$ek1lk7!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition

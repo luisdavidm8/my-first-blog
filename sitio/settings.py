0f08149c9c99fc2a0dbef630351049662885a543
luisdavidm8 <lmercadom21@curnvirtual.edu.co>
2024-04-16 14:59:50 -0500

    Templates modificado para mostar lo que se publique

--- sitio/settings.py
+++ sitio/settings.py
@@ -93,9 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-co'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Bogota'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
